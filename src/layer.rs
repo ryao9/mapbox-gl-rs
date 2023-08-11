f335731f2d460351f1af304fb4d2f460559e62a9
Ryan Yao <ryan.yao@maymobility.com>
2023-08-11 10:45:34 -0400

    Add more layer fields

--- src/layer.rs
+++ src/layer.rs
@@ -78,11 +78,29 @@ pub struct Layout {
     pub icon_image: Option<LayoutProperty>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub icon_size: Option<LayoutProperty>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_anchor: Option<LayoutProperty>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_field: Option<LayoutProperty>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_offset: Option<(i32, i32)>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_size: Option<LayoutProperty>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub icon_allow_overlap: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_allow_overlap: Option<bool>,
 }
 
 #[derive(Debug, Clone, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Paint {
-    pub line_color: String,
-    pub line_width: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub line_color: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub line_width: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_halo_width: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_halo_color: Option<String>,
 }
